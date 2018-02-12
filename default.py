546c7530dff613f243e17fdcfb0bbee9ce646855
Timan Rebel <timan@trebel.nl>
2018-02-12 15:57:37 +0100

    Update plugin to work with newest Kodi in car and also store playlists

--- default.py
+++ default.py
@@ -16,6 +16,7 @@
 import os
 import xbmc
 import xbmcaddon
+import json
 from time import sleep
 
 
@@ -24,6 +25,7 @@ ADDON_ID = ADDON.getAddonInfo('id')
 FOLDER = ADDON.getSetting('autoresume.save.folder').encode('utf-8', 'ignore')
 FREQUENCY = int(ADDON.getSetting('autoresume.frequency'))
 PATH = os.path.join(FOLDER, 'autoresume.txt')
+PATHLIST = os.path.join(FOLDER, 'autoresume.m3u')
 
 def resume():
   for x in range(0,120):
@@ -32,40 +34,79 @@ def resume():
         # Read from autoresume.txt.
         f = open(PATH, 'r')
         mediaFile = f.readline().rstrip('\n')
-        position = float(f.readline())
+        position = 0
+        playlistPos = 0
+
+        if mediaFile:
+          position = float(f.readline())
+          playlistPos = int(f.readline())
         f.close()
-        # Play file.
-        xbmc.Player().play(mediaFile)
-        while (not xbmc.Player().isPlaying()):
-          sleep(0.5)
-        sleep(1)
-        # Seek to last recorded position.
-        xbmc.Player().seekTime(position)
-        sleep(1)
-        # Make sure it actually got there.
-        if abs(position - xbmc.Player().getTime()) > 30:
-          xbmc.Player().seekTime(position)
+
+        if playlistPos  > 0:
+            # Load playlist
+            log("Playlist ************************")
+            if os.path.exists(PATHLIST):
+                log("Playlist loaded")
+                log(PATHLIST)
+                result = xbmc.PlayList(xbmc.PLAYLIST_MUSIC).load(PATHLIST)
+                log(result)
+
+                xbmc.Player().playselected(playlistPos)
+                xbmc.Player().pause()
+                while (not xbmc.Player().isPlaying()):
+                  sleep(0.5)
+                sleep(0.5)
+                xbmc.Player().seekTime(position)
+        else:
+            # Play file.
+            xbmc.Player().play(mediaFile)
+            xbmc.Player().pause()
+            while (not xbmc.Player().isPlaying()):
+              sleep(0.5)
+            sleep(1)
+            # Seek to last recorded position.
+            xbmc.Player().seekTime(position)
+            sleep(1)
+            # Make sure it actually got there.
+            if abs(position - xbmc.Player().getTime()) > 30:
+              xbmc.Player().seekTime(position)
+
       break
     else:
       # If the folder didn't exist maybe we need to wait longer for the drive to be mounted.
       sleep(5)
 
 def recordPosition():
-  if xbmc.Player().isPlaying():
+  if xbmc.Player().isPlayingAudio():
     mediaFile = xbmc.Player().getPlayingFile()
     position = xbmc.Player().getTime()
+    playlistPos = xbmc.PlayList(xbmc.PLAYLIST_MUSIC).getposition()
+
     # Write info to file
     f = open(PATH, 'w')
     f.write(mediaFile)
     f.write('\n')
     f.write(repr(position))
+    f.write('\n')
+    f.write(repr(playlistPos))
+    f.write('\n')
+
     f.close()
-  else:
-    if os.path.exists(PATH):
-      os.remove(PATH)
+
+    json_string = xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "Playlist.GetItems", "params": { "properties": ["file"], "playlistid": 0 }, "id": 1}')
+    parsed_json = json.loads(json_string)
+
+    if parsed_json['result']['limits']['total'] > 0:
+        f = open(PATHLIST, 'w')
+
+        for item in parsed_json['result']['items']:
+            f.write(item['file'].encode('utf8'))
+            f.write('\n')
+
+        f.close()
 
 def log(msg):
-  xbmc.log("%s: %s" % (ADDON_ID, msg), xbmc.LOGDEBUG)
+  xbmc.log("%s: %s" % (ADDON_ID, msg), xbmc.LOGNOTICE)
 
 
 if __name__ == "__main__":
